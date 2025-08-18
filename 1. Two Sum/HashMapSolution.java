8f39fcf2794d54e74f06e1a9b7904fa60896bbec
Avinash Pandey <63946706+avi714@users.noreply.github.com>
2025-08-18 20:33:27 +0530

    Add files via upload

---
+++ 1. Two Sum/HashMapSolution.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        HashMap<Integer, Integer> frequency = new HashMap<>();
+        int[] result = new int[2];
+
+        for (int i = 0; i <nums.length; i++)
+        {
+            int tempResult = target - nums[i];
+            if (frequency.containsKey(nums[i]))
+            {
+                result[0] = frequency.get(nums[i]);
+                result[1] = i;
+                return result;
+            }
+            else
+            {
+                frequency.put(tempResult,i);
+            }
+        }
+	
+	//Just to avoid compile time error of return type being an array for the function. Although we are returning above the result array we still need to include a return statement for the function at the end.
+        return new int[] {};
+    }
+}
